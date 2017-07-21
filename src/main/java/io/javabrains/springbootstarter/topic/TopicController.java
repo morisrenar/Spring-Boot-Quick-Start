b280ee9a7ae2d7c5176d8868fbd8db7fbb5ab13f
bmshamsnahid <morisrenar@gmail.com>
2017-07-21 21:39:20 +0600

    19: creating a new resource using post method

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,9 +1,7 @@
 package io.javabrains.springbootstarter.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
@@ -26,5 +24,10 @@ public class TopicController {
 	@RequestMapping("/topics/{id}")
 	public Topic getTopic(@PathVariable String id) {
 		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
 	}
 }
--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -2,6 +2,7 @@ package io.javabrains.springbootstarter.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,11 +11,11 @@ import java.util.List;
  */
 @Service
 public class TopicService {
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<Topic>(Arrays.asList(
 			new Topic("spring", "Spring Framework", "Spring Framework Description"),
 			new Topic("Java", "Core Java", "Core java Description"),
 			new Topic("Java Script", "Java Script", "Java Script Description")
-	);
+	));
 	
 	public List<Topic> getAllTopics() {
 		return topics;
@@ -27,5 +28,16 @@ public class TopicService {
 			}
 		}
 		return null;
+	}
+	
+	public void addTopic(Topic topic) {
+		if(topic != null) {
+			System.out.println("New Topic id: " + topic.getId());
+			System.out.println("New Topic name: " + topic.getName());
+			System.out.println("New Topic description: " + topic.getDescription());
+		} else {
+			System.out.println("New Topic is null");
+		}
+		topics.add(topic);
 	}
 }
