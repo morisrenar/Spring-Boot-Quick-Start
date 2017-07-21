f72ebf5e332cf5f0599fcfef3ba457cfe774bc94
bmshamsnahid <morisrenar@gmail.com>
2017-07-21 17:04:13 +0600

    11: adding a rest controller

---
+++ src/main/java/io/javabrains/springbootstarter/CourseApiApp.java
@@ -0,0 +1,15 @@
+package io.javabrains.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by bmshamsnahid on 7/21/17.
+ */
+@SpringBootApplication
+public class CourseApiApp {
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApp.class, args);
+	}
+	
+}
